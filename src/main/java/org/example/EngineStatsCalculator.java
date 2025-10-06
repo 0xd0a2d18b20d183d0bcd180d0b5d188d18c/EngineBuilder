6bf94ed1c388696140a00fef940cb2208e2c9790
Oleg N <4314336-0xd0a2d18b20d183d0bcd180d0b5d188d18c@users.noreply.gitlab.com>
2025-10-07 00:15:59 +0600

    update EngineStatsCalculator: simplifying

--- src/main/java/org/example/EngineStatsCalculator.java
+++ src/main/java/org/example/EngineStatsCalculator.java
@@ -2,28 +2,28 @@ package org.example;
 
 public class EngineStatsCalculator {
 
-  static double calcDisplacementPerCyl(double bore, double stroke) {
-    double pi = 0.7853;
-    double boreC = bore * bore;
-    double sq = pi * boreC * stroke;
-    double next = sq / 1000000;
-    int first3Digits = (int)(next * 1000) % 1000;
-    return (double) first3Digits / 1000;
+  // return cubic mm
+  static int calcDisplacementPerCyl(int bore, int stroke) {
+    return Math.toIntExact(Math.round(Math.PI * stroke * Math.pow((double) bore / 2, 2)));
   }
 
-  static double calcDisplacement(double cylinderDisplacement, int cylinders) {
+  // return cubic mm
+  static int calcDisplacement(int cylinderDisplacement, int cylinders) {
     return cylinderDisplacement * cylinders;
   }
 
+  // return Nm * m
   static int calcTorque(double displacement, int mep) {
     // 1000 / 2pi / 2 (4 stroke) = 79.5
-    return (int) ((displacement / 1000) * mep * 79.5);
+    return (int) ((displacement / 1000000000) * mep * 79.5);
   }
 
+  // return kwt
   static int calcPower(int torque, int rpm) {
     return (torque * rpm) / 9550;
   }
 
+  // return hp
   static int calcHorsepower(int kwt) {
     return Math.toIntExact((long) (kwt * 1.341));
   }
